734e27ac00e872e60d7d73207822c609ea7cac42
Fenext04 <mateusmelo04@gmail.com>
2022-09-22 14:39:13 -0300

    first commit

---
+++ patricia/__init__.py
@@ -0,0 +1,10 @@
+from patriciatree import *
+
+
+arvore = Arvore("casco")
+
+
+
+
+
+print(arvore.raiz)
---
+++ patricia/node.py
@@ -0,0 +1,48 @@
+class Node:
+
+    def __init__(self, pos, val, prefixo):
+
+        """
+        Unidade basica
+        :param int pos:
+        :param str val:
+        :param str prefixo:
+        """
+
+        self.filhos = [None, None]
+
+        self.prefixo = prefixo
+
+        self.pos = pos
+
+        self.val = ord(val)
+
+    def get(self, palavra):
+
+        if palavra[:self.pos] != self.prefixo or len(palavra) <= self.pos: return 0
+
+        valor = ord(palavra[self.pos])
+
+        if valor <= self.val:
+
+            return self.filhos[0]
+
+        return self.filhos[1]
+
+
+    def derivados(self):
+
+        derivados = []
+
+        for i in self.filhos:
+
+            if type(i) == str: derivados.append(i[:-1])
+
+            else: derivados += i.derivados()
+
+        return derivados
+
+
+    def __repr__(self):
+
+        return "[{}|{}]".format(self.pos, chr(self.val))
